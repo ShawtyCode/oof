cb6865d45643e0684a68ea9a89c8b46fd148b99f
ShawtyCode <40865968+ShawtyCode@users.noreply.github.com>
2018-07-13 23:36:37 +0200

    Update actualbot.js

--- actualbot.js
+++ actualbot.js
@@ -2,13 +2,16 @@ const discord = require("discord.js");
 
 const bot = new discord.Client();
 
+// add message as a parameter to your callback function
 bot.on('message', function(message) {
-	if (message.content === "c!bump") {
-		var interval = setInterval (function () {
-			message.channel.send("!disboard bump")
-			catch(console.error)
-		}, 1 * 1);
-		{
+    // Now, you can use the message variable inside
+    if (message.content === "c!autobump") { 
+        var interval = setInterval (function () {
+            // use the message's channel (TextChannel) to send a new message
+            message.channel.send("!disboard bump")
+            .catch(console.error); // add error handling here
+        }, 1 * 7200000);
+		
 
 
 client.login(process.env.BOT_TOKEN);
