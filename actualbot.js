35bb0b8e0f89fb89c31ad5019071631b3fa54635
ShawtyCode <40865968+ShawtyCode@users.noreply.github.com>
2018-07-13 23:33:18 +0200

    Update actualbot.js

--- actualbot.js
+++ actualbot.js
@@ -1,17 +1,17 @@
 const discord = require("discord.js");
 
-const bot = new discord.Client
+const bot = new discord.Client();
 
 bot.on('message', function(message) {
 	if (message.content === "c!bump") {
-		var interval = setInterval (function() {
+		var interval = setInterval (function () {
 			message.channel.send("!disboard bump")
-		//	.catch(console.error)
-		}, 1 * 2); // 7200000 (120min)
+			catch(console.error)
+			}, 1 * 1);
 		{
 
 
-client.login(TOKEN)
+client.login(process.env.BOT_TOKEN);
 
 
 
